0b631f39d6247c682ce545db210cc095a6a77e5c
Hendra Dwi Saputra <babeguru@gmail.com>
2025-07-06 22:11:53 +0700

    Refactor theme handling and update UI elements for clarity

--- ui/main.py
+++ ui/main.py
@@ -3,10 +3,14 @@ import flet as ft
 
 class Main:
     def __call__(self, page: ft.Page):
+        page.title = "GADIS | Gambar Medis"
         page.window_width = 640
         page.window_height = 480
+
         page.theme_mode = ft.ThemeMode.LIGHT
-        
+        page.theme = ft.Theme(color_scheme_seed=ft.colors.GREEN)
+        page.dark_theme = ft.Theme(color_scheme_seed=ft.colors.GREEN)
+
         theme_icon = ft.IconButton(
             icon=ft.icons.DARK_MODE,
             on_click=lambda e: toggle_theme(),
@@ -14,29 +18,30 @@ class Main:
         )
 
         def update_theme_icon():
-            """Update ikon berdasarkan tema saat ini"""
+            """Update the icon based on the current theme"""
             if page.theme_mode == ft.ThemeMode.DARK:
                 theme_icon.icon = ft.icons.LIGHT_MODE
-                theme_icon.icon_color = "yellow"
+                theme_icon.icon_color = ft.colors.YELLOW
             else:
                 theme_icon.icon = ft.icons.DARK_MODE
                 theme_icon.icon_color = None
             page.update()
 
         def toggle_theme():
-            """Ganti tema dan update ikon"""
+            """Switch theme and update icon"""
             page.theme_mode = (
-                ft.ThemeMode.DARK 
-                if page.theme_mode == ft.ThemeMode.LIGHT 
+                ft.ThemeMode.DARK
+                if page.theme_mode == ft.ThemeMode.LIGHT
                 else ft.ThemeMode.LIGHT
             )
             update_theme_icon()
 
+        tl = ft.Text(value="Gambar Medis", color=ft.colors.PRIMARY, size=24)
+        st = ft.Text(value="Rekam Medis Radiologi", color=ft.colors.SECONDARY, size=12)
+
         update_theme_icon()
-
-        t = ft.Text(value="Hello, world!", color="green")
-
         page.controls.clear()
         page.controls.append(theme_icon)
-        page.controls.append(t)
+        page.controls.append(tl)
+        page.controls.append(st)
         page.update()
