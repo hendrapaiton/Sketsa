7ff136f548485766a53342c470d0d5465ca58076
Hendra Dwi Saputra <babeguru@gmail.com>
2025-07-06 10:13:26 +0700

    Implement theme toggle functionality with icon update

--- ui/main.py
+++ ui/main.py
@@ -1,7 +1,42 @@
 import flet as ft
 
+
 class Main:
     def __call__(self, page: ft.Page):
+        page.window_width = 640
+        page.window_height = 480
+        page.theme_mode = ft.ThemeMode.LIGHT
+        
+        theme_icon = ft.IconButton(
+            icon=ft.icons.DARK_MODE,
+            on_click=lambda e: toggle_theme(),
+            icon_size=24,
+        )
+
+        def update_theme_icon():
+            """Update ikon berdasarkan tema saat ini"""
+            if page.theme_mode == ft.ThemeMode.DARK:
+                theme_icon.icon = ft.icons.LIGHT_MODE
+                theme_icon.icon_color = "yellow"
+            else:
+                theme_icon.icon = ft.icons.DARK_MODE
+                theme_icon.icon_color = None
+            page.update()
+
+        def toggle_theme():
+            """Ganti tema dan update ikon"""
+            page.theme_mode = (
+                ft.ThemeMode.DARK 
+                if page.theme_mode == ft.ThemeMode.LIGHT 
+                else ft.ThemeMode.LIGHT
+            )
+            update_theme_icon()
+
+        update_theme_icon()
+
         t = ft.Text(value="Hello, world!", color="green")
+
+        page.controls.clear()
+        page.controls.append(theme_icon)
         page.controls.append(t)
         page.update()
